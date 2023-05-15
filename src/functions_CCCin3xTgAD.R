e2b91f520990022bc2b11d075a6cdbf97f2d300f
Tabea Marga Soelter <tsoelter@c0195.cm.cluster>
2023-05-15 09:39:56 -0500

    initial commit of project functions

---
+++ src/functions_CCCin3xTgAD.R
@@ -0,0 +1,282 @@
+### Functions for CCC in 3xTgAD project 
+# Tabea M. Soelter 
+
+## make_seurat_object
+# A function which takes a path to sample folders with the three CellRanger output files and creates a merged seurat object
+make_seurat_object <- function(path){
+  counts_list <- list.dirs(path, 
+                           full.names = TRUE, 
+                           recursive = FALSE)
+  object_list <- vector('list')
+  for (i in counts_list){
+    counts <- Read10X(i)
+    print(i)
+    sample_name <- basename(i)
+    sample_name <- gsub("[[:punct:]]", "", sample_name)
+    seurat_object <- CreateSeuratObject(counts = counts, project = sample_name, min.features = 200)
+    if (str_sub(sample_name, - 2, - 1) == "AD") {
+      seurat_object$orig.ident <- "AD"
+      if (str_sub(sample_name, - 4, - 1) == "6mAD") {
+        seurat_object$timepoint <- "6m"
+      } else {
+        seurat_object$timepoint <- "12m"
+      }
+    } else {
+      seurat_object$orig.ident <- "CTRL"
+      if (str_sub(sample_name, - 4, - 3) == "6m") {
+        seurat_object$timepoint <- "6m"
+      } else {
+        seurat_object$timepoint <- "12m"
+      }
+    }
+    object <- seurat_object
+    object_list[[i]] <- object
+  } 
+  print("Making 6m diseased Seurat Object")
+  AD_list <- object_list[grepl("6m_AD", names(object_list))]
+  for (i in names(AD_list)) {
+    sample_name <- basename(i)
+    sample_name <- gsub("[[:punct:]]", "", sample_name)
+    AD_list[[i]] <- RenameCells(AD_list[[i]],
+                                add.cell.id = sample_name)
+  }
+  diseased_6m <- Merge_Seurat_List(AD_list)
+  
+  print("Making 12m diseased Seurat Object")
+  AD_list <- object_list[grepl("12m_AD", names(object_list))]
+  for (i in names(AD_list)) {
+    sample_name <- basename(i)
+    sample_name <- gsub("[[:punct:]]", "", sample_name)
+    AD_list[[i]] <- RenameCells(AD_list[[i]],
+                                add.cell.id = sample_name)
+  }
+  diseased_12m <- Merge_Seurat_List(AD_list)
+  
+  print("Merging 6m and 12m diseased objects")
+  merged_seurat_AD <- merge(x = diseased_6m,
+                            y = diseased_12m,
+                            add.cell.id = c("6m", "12m"))
+  
+  print("Making 6m control Seurat Object")
+  CTRL_list <- object_list[grepl("6m_WT", names(object_list))]
+  for (i in names(CTRL_list)) {
+    sample_name <- basename(i)
+    sample_name <- gsub("[[:punct:]]", "", sample_name)
+    CTRL_list[[i]] <- RenameCells(CTRL_list[[i]],
+                                  add.cell.id = sample_name)
+  }
+  control_6m <- Merge_Seurat_List(CTRL_list)
+  
+  print("Making 12m control Seurat Object")
+  CTRL_list <- object_list[grepl("12m_WT", names(object_list))]
+  for (i in names(CTRL_list)) {
+    sample_name <- basename(i)
+    sample_name <- gsub("[[:punct:]]", "", sample_name)
+    CTRL_list[[i]] <- RenameCells(CTRL_list[[i]],
+                                  add.cell.id = sample_name)
+  }
+  control_12m <- Merge_Seurat_List(CTRL_list)
+  
+  print("Merging 6m and 12m control objects")
+  merged_seurat_CTRL <- merge(x = control_6m,
+                            y = control_12m,
+                            add.cell.id = c("6m", "12m"))
+  
+  print("Making merged Seurat Object")
+  merged_seurat <- merge(x = merged_seurat_CTRL,
+                         y = merged_seurat_AD,
+                         add.cell.id = c("CTRL", "AD"))
+  return(merged_seurat)
+}
+
+## calculate_qc
+# A function which calculates quality control metrics for a merged seurat object
+calculate_qc <- function(seurat_object){
+  seurat_object$log10GenesPerUMI <- log10(seurat_object$nFeature_RNA) / log10(seurat_object$nCount_RNA)
+  seurat_object$mitoRatio <- PercentageFeatureSet(object = seurat_object, 
+                                                  pattern = "^mt-")
+  seurat_object$mitoRatio <- seurat_object@meta.data$mitoRatio / 100
+  return(seurat_object)
+}
+
+## format_metadata
+# A function which extracts and formats metadata of a seurat object
+format_metadata <- function(seurat_object){
+  metadata <- seurat_object@meta.data
+  metadata$cells <- rownames(metadata)
+  # Create sample column -----
+  metadata$sample <- NA
+  metadata$sample[which(str_detect(metadata$cells, "^CTRL_"))] <- "ctrl"
+  metadata$sample[which(str_detect(metadata$cells, "^AD_"))] <- "AD"
+  # Rename columns -----
+  metadata <- metadata %>% dplyr::rename(seq_folder = orig.ident,
+                                         nUMI = nCount_RNA,
+                                         nGene = nFeature_RNA)
+  return(metadata)
+}
+
+## plot_qc
+# A function which takes seurat metadata and plots quality control metrics for filtering purposes
+plot_qc <- function(metadata) {
+  # Visualize the number of cell counts per sample
+  number_of_cells <- metadata %>% 
+    ggplot(aes(x = sample, fill = sample)) + 
+    geom_bar() +
+    theme_classic() +
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+    theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
+    ggtitle("NCells") 
+  # Visualize the number UMIs/transcripts per cell
+  number_of_umis <- metadata %>% 
+    ggplot(aes(color = sample, x = nUMI, fill = sample)) + 
+    geom_density(alpha = 0.2) + 
+    scale_x_log10() + 
+    theme_classic() +
+    ylab("Cell density") +
+    geom_vline(xintercept = 500)
+  # Visualize the distribution of genes detected per cell
+  dist_genes_per_cell <- metadata %>% 
+    ggplot(aes(color = sample, x = nGene, fill = sample)) + 
+    geom_density(alpha = 0.2) + 
+    theme_classic() +
+    scale_x_log10() + 
+    geom_vline(xintercept = 300)
+  # Visualize the overall complexity of the gene expression by visualizing the genes detected per UMI (novelty score)
+  novelty_score <- metadata %>%
+    ggplot(aes(x = log10GenesPerUMI, color = sample, fill = sample)) +
+    geom_density(alpha = 0.2) +
+    theme_classic() +
+    geom_vline(xintercept = 0.8)
+  # Visualize the distribution of mitochondrial gene expression detected per cell
+  dist_mito_gex <- metadata %>% 
+    ggplot(aes(color = sample, x = mitoRatio, fill = sample)) + 
+    geom_density(alpha = 0.2) + 
+    scale_x_log10() + 
+    theme_classic() +
+    geom_vline(xintercept = 0.2)
+  # Visualize the correlation between genes detected and number of UMIs and determine whether strong presence of cells   with low numbers of genes/UMIs
+  cor <- metadata %>% 
+    ggplot(aes(x = nUMI, y = nGene, color = mitoRatio)) + 
+    geom_point() + 
+    scale_colour_gradient(low = "gray90", high = "black") +
+    stat_smooth(method = lm) +
+    scale_x_log10() + 
+    scale_y_log10() + 
+    theme_classic() +
+    geom_vline(xintercept = 500) +
+    geom_hline(yintercept = 250) +
+    facet_wrap(~sample)
+  # Plot QC metrics
+  plot(number_of_cells) 
+  plot(number_of_umis)
+  plot(dist_genes_per_cell)
+  plot(novelty_score)
+  plot(dist_mito_gex)
+  plot(cor)
+}
+
+## convert_human_gene_list
+# Source: https://www.r-bloggers.com/2016/10/converting-mouse-to-human-gene-names-with-biomart-package/ 
+# I adapted the function to use an archived version of ensembl, as there were mirror issues due to update in Feb 2023
+# A function to convert human to mouse gene names 
+convert_human_gene_list <- function(x) {
+  require("biomaRt")
+  human = useMart("ensembl",
+                  dataset = "hsapiens_gene_ensembl",
+                  host = "https://dec2021.archive.ensembl.org/")
+  mouse = useMart("ensembl",
+                  dataset = "mmusculus_gene_ensembl",
+                  host = "https://dec2021.archive.ensembl.org/")
+  genesV2 = getLDS(attributes = c("hgnc_symbol"),
+                   filters = "hgnc_symbol",
+                   values = x ,
+                   mart = human,
+                   attributesL = c("mgi_symbol"),
+                   martL = mouse,
+                   uniqueRows = T)
+  humanx <- unique(genesV2[, 2])
+  return(humanx)
+}
+
+
+## cell_cycle_effects
+# A function which calculates and plots the effect of cell cycle on the data using a filtered seurat object as input. It also performs log normalization, scaling, and dimension reduction using PCA
+cell_cylce_effects <- function(filtered_seurat){
+  # log normalize -----
+  filtered_seurat <- NormalizeData(filtered_seurat)
+  # convert human cell cylce markers to mouse -----
+  s.genes <- convert_human_gene_list(cc.genes.updated.2019$s.genes)
+  g2m.genes <- convert_human_gene_list(cc.genes.updated.2019$g2m.genes)
+  # score cells based in gex of genes -----
+  filtered_seurat <- CellCycleScoring(filtered_seurat,
+                                      g2m.features = g2m.genes,
+                                      s.features = s.genes)
+  filtered_seurat <- FindVariableFeatures(filtered_seurat,
+                                          selection.method = "vst",
+                                          verbose = FALSE)
+  # scale data -----
+  filtered_seurat <- ScaleData(filtered_seurat)
+  # run pca -----
+  filtered_seurat <- RunPCA(filtered_seurat, approx = FALSE)
+  # plot pca -----
+  elbow <- ElbowPlot(filtered_seurat, reduction = "pca", ndims = 50)
+  # plot cell cycle scoring -----
+  cell_cycle_plot <- DimPlot(filtered_seurat,
+                             reduction = "pca",
+                             group.by = "Phase",
+                             split.by = "Phase")
+  plot(cell_cycle_plot)
+  plot(elbow)
+  return(filtered_seurat)
+}
+
+## harmony_integration
+# A function which integrates a seurat object using harmony
+harmony_integration <- function(seurat_object, dims){
+  seurat_object <- RunHarmony(seurat_object,
+                              group.by.vars = "sample",
+                              reduction = "pca",
+                              dims.use = dims, assay.use = "RNA")
+  # Here we use all PCs computed from Harmony for UMAP calculation -----
+  seurat_object <- RunUMAP(seurat_object, dims = dims, reduction = "harmony", reduction.name = "umap_harmony")
+  return(seurat_object)
+}
+
+## find_clusters
+# A function which finds clusters for a Seurat Object at user determined resolutions
+find_clusters <- function(object, dims, reduction, resolutions) {
+  # set reduction method to harmony -----
+  object <- FindNeighbors(object, 
+                          dims = dims, 
+                          reduction = reduction)
+  # clustering (Leiden aka algorithm 4)
+  for (res in resolutions) {
+    object <- FindClusters(object,
+                           graph.name = "RNA_snn",
+                           resolution = res,
+                           algorithm = 4,
+                           method = "igraph")
+  } 
+  return(object)
+}
+
+## find_markers
+# A function allowing for individual FindMarkers analyses for a defined list of clusters
+find_markers <- function(object, resolution, identities, value){
+  # set resolution ----------
+  object <- SetIdent(object, value = resolution)
+  # create empty df ----------
+  top_markers <- data.frame()
+  # iterating through all unidentified clusters ----------
+  for (i in identities) {
+    # find marker genes ----------
+    markers <- FindMarkers(object, ident.1 = i, max.cells.per.ident = 100, logfc.threhold = 0.25, only.pos = TRUE)
+    # print out which cluster was completed ----------
+    print(paste0("Markers for cluster ", i))
+    # filter markers by specified value and adjusted p-value ----------
+    markers_filt <- markers %>% top_n(-value, p_val_adj) %>% add_column(cluster = i)
+    # bind empty df and filtered markers together ----------
+    top_markers <- rbind(top_markers, markers_filt)
+  }
+  return(top_markers)
+}
